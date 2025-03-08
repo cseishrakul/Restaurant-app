593defb3ba321db3118ee7fa3b1576292b781e8b
cseishrakul <ishrakul236@gmail.com>
2025-03-09 01:13:41 +0600

    Navbar design under process

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <img src={assets.logo} className="logo" alt="" />
+      <ul className="navbar-menu">
+        <li onClick={()=>setMenu('home')} className={menu==='home'?'active':''}>Home</li>
+        <li onClick={()=>setMenu('items')} className={menu==='items'?'active':''}>Items</li>
+        <li onClick={()=>setMenu('app')} className={menu==='app'?'active':''}>App</li>
+        <li onClick={()=>setMenu('contact-us')} className={menu==='contact-us'?'active':''}>Contact Us</li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
